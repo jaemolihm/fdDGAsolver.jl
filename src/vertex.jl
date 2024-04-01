cada0fc3301802c8cfd19363ba1df19017ed761a
Jae-Mo Lihm <jaemolim96@gmail.com>
2024-04-02 00:42:42 +0200

    Implement K2, K2p and K3 vertices

--- src/vertex.jl
+++ src/vertex.jl
@@ -4,7 +4,7 @@ abstract type AbstractVertex{DD, Q <: Number} end
 channel_freq(v::AbstractVertex) = v.channel_freq
 
 """
-    Vertex_K1{1, Q <: Number, MT <: NTuple{1, Mesh}, AT <: AbstractArray{Q, DD}, MF <: MeshFunction{1, Q, MT, AT}}
+    Vertex_K1{DD, Q <: Number, MT <: NTuple{1, Mesh}, AT <: AbstractArray{Q, DD}, MF <: MeshFunction{1, Q, MT, AT}}
 
 Vertex_K1 type with fields:
 * `data :: MF` : `MeshFunction` storing the data
@@ -17,25 +17,49 @@ struct Vertex_K1{DD, Q <: Number, MT <: NTuple{DD, Mesh}, AT <: AbstractArray{Q,
     lim :: Q
 end
 
+struct Vertex_K2{DD, Q <: Number, MT <: NTuple{DD, Mesh}, AT <: AbstractArray{Q, DD}, MF <: MeshFunction{DD, Q, MT, AT}} <: AbstractVertex{DD, Q}
+    data :: MF
+    channel_freq :: Symbol
+    lim :: Q
+end
 
-function Base.show(io::IO, v::Vertex_K1)
+struct Vertex_K2p{DD, Q <: Number, MT <: NTuple{DD, Mesh}, AT <: AbstractArray{Q, DD}, MF <: MeshFunction{DD, Q, MT, AT}} <: AbstractVertex{DD, Q}
+    data :: MF
+    channel_freq :: Symbol
+    lim :: Q
+end
+
+struct Vertex_K3{DD, Q <: Number, MT <: NTuple{DD, Mesh}, AT <: AbstractArray{Q, DD}, MF <: MeshFunction{DD, Q, MT, AT}} <: AbstractVertex{DD, Q}
+    data :: MF
+    channel_freq :: Symbol
+    lim :: Q
+end
+
+
+function Base.show(io::IO, v::AbstractVertex)
     print(io, "$(nameof(typeof(v))), channel_freq = $(v.channel_freq)\n")
-    print(io, "meshes:\n")
+    print(io, "meshes:")
     for (i, mesh) in enumerate(v.data.meshes)
-        print(io, "    $i. $(typeof(value(first(mesh.points)))), N = $(mesh.domain[:N])")
+        print(io, "\n    $i. $(typeof(value(first(mesh.points)))), N = $(mesh.domain[:N])")
     end
 end
+
+Base.size(v :: AbstractVertex) = length.(meshes(v.data))
 
 Base.getindex(f :: AbstractVertex{DD}, x :: Vararg{Any, DD}) where {DD} = getindex(f.data, x...)
 
-function (f :: AbstractVertex)(v1, v2, w, channel_freq :: Symbol = f.channel_freq)
-    if channel_freq === f.channel_freq
-        f.data(w; lim = f.lim)
-    else
-        v1_, v2_, w_ = convert_channel(f.channel_freq, channel_freq, v1, v2, w)
-        f.data(w_; lim = f.lim)
-    end
+function (f :: AbstractVertex)(v1, v2, w, channel_freq :: Symbol)
+    v1_, v2_, w_ = convert_channel(f.channel_freq, channel_freq, v1, v2, w)
+    f(v1_, v2_, w_)
 end
 
+(f :: Vertex_K1)(v1, v2, w) = f.data(w; lim = f.lim)
+(f :: Vertex_K2)(v1, v2, w) = f.data(v1, w; lim = f.lim)
+(f :: Vertex_K2p)(v1, v2, w) = f.data(v2, w; lim = f.lim)
+(f :: Vertex_K3)(v1, v2, w) = f.data(v1, v2, w; lim = f.lim)
+
 export
-    Vertex_K1
+    Vertex_K1,
+    Vertex_K2,
+    Vertex_K2p,
+    Vertex_K3
--- test/test_vertex.jl
+++ test/test_vertex.jl
@@ -18,4 +18,33 @@ using Test
     @test y(v1, v2, w, :p) == x(-v1 - v2 - w)
     @test y(v1, v2, w, :t) == x(v1 - v2)
     @test y(v1, v2, 10_000.) == 3.
+
+    n = length(mesh)
+    x_K2 = MeshFunction((mesh, mesh), rand(n, n))
+    x_K3 = MeshFunction((mesh, mesh, mesh), rand(n, n, n))
+    y_K2  = Vertex_K2( x_K2, :a, 5.)
+    y_K2p = Vertex_K2p(x_K2, :a, 6.)
+    y_K3  = Vertex_K3( x_K3, :a, 7.)
+    @test size(y_K2)  === (n, n)
+    @test size(y_K2p) === (n, n)
+    @test size(y_K3)  === (n, n, n)
+
+    v1, v2, w = 0., 1., 3.
+    @test y_K2.data === y_K2p.data
+    @test y_K2(v1, v2, w) ==  y_K2p(v2, v1, w)
+    @test y_K2(10_000., v1, w) == 5.0
+    @test y_K2p(v1, 10_000., w) == 6.0
+    @test y_K2(v1, 10_000., w) == x_K2(v1, w)
+    @test y_K2p(10_000., v1, w) == x_K2(v1, w)
+    @test y_K2(v1, v2, w, :p) == x_K2(v1, -v1 - v2 - w)
+    @test y_K2(v1, v2, w, :t) == x_K2(v2+w, v1-v2)
+    @test y_K3(v1, v2, w) == x_K3(v1, v2, w)
+    @test y_K3(v1, v2, w, :a) == x_K3(v1, v2, w)
+    @test y_K3(v1, v2, w, :p) == x_K3(v1, v2, -v1 - v2 - w)
+    @test y_K3(v1, v2, w, :t) == x_K3(v2+w, v2, v1-v2)
+
+    v1, v2, v3 = mesh[5], mesh[8], mesh[10]
+    @test y_K2[v1, v3] == x_K2[v1, v3]
+    @test y_K2p[v2, v3] == x_K2[v2, v3]
+    @test y_K3[v1, v2, v3] == x_K3[v1, v2, v3]
 end
