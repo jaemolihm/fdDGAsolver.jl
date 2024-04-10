541e40f1a0c0915e878fee2147f9dd5de8f4950a
Dominik Kiese <dkiese1308.physik@gmail.com>
2024-04-10 14:24:24 -0400

    tweaks

--- src/convention.jl
+++ src/convention.jl
@@ -1,20 +1,23 @@
+# conversion between different frequency/momentum representations
+#----------------------------------------------------------------------------------------------#
 
 """
     function convert_frequency(
-    Ω  :: MatsubaraFrequency{Boson},
-    ν  :: Union{InfiniteMatsubaraFrequency, MatsubaraFrequency{Fermion}},
-    νp :: Union{InfiniteMatsubaraFrequency, MatsubaraFrequency{Fermion}},
-       :: Type{Ch_from},
-       :: Type{Ch_to},
-    ) where {Ch_from <: ChannelTag, Ch_to <: ChannelTag}
-Convert frequencies in the `Ch_from` channel representation to `Ch_to` channel representation.
+        Ω  :: MatsubaraFrequency{Boson},
+        ν  :: Union{InfiniteMatsubaraFrequency, MatsubaraFrequency{Fermion}},
+        νp :: Union{InfiniteMatsubaraFrequency, MatsubaraFrequency{Fermion}},
+           :: Type{Ch_from},
+           :: Type{Ch_to}
+        ) where {Ch_from <: ChannelTag, Ch_to <: ChannelTag}
+
+Convert frequencies in the `Ch_from` channel representation to `Ch_to` channel representation
 """
 @inline function convert_frequency(
     Ω  :: MatsubaraFrequency{Boson},
     ν  :: Union{InfiniteMatsubaraFrequency, MatsubaraFrequency{Fermion}},
     νp :: Union{InfiniteMatsubaraFrequency, MatsubaraFrequency{Fermion}},
        :: Type{Ch_from},
-       :: Type{Ch_to},
+       :: Type{Ch_to}
     ) where {Ch_from <: ChannelTag, Ch_to <: ChannelTag}
 
     if Ch_from === Ch_to
@@ -39,44 +42,51 @@ Convert frequencies in the `Ch_from` channel representation to `Ch_to` channel r
         return ν - νp, νp, Ω + νp
 
     else
-        throw(ArgumentError("Wrong channels $Ch_from or $Ch_to"))
+        throw(ArgumentError("Cannot convert from $Ch_from to $Ch_to!"))
     end
-
 end
 
+"""
+    function convert_momentum(
+        Q  :: BrillouinPoint,
+        k  :: BrillouinPoint,
+        kp :: BrillouinPoint,
+           :: Type{Ch_from},
+           :: Type{Ch_to}
+        ) where {Ch_from <: ChannelTag, Ch_to <: ChannelTag}
+
+Convert momenta in the `Ch_from` channel representation to `Ch_to` channel representation
+"""
 @inline function convert_momentum(
-    Ω  :: BrillouinPoint,
-    ν  :: BrillouinPoint,
-    νp :: BrillouinPoint,
+    Q  :: BrillouinPoint,
+    k  :: BrillouinPoint,
+    kp :: BrillouinPoint,
        :: Type{Ch_from},
-       :: Type{Ch_to},
+       :: Type{Ch_to}
     ) where {Ch_from <: ChannelTag, Ch_to <: ChannelTag}
 
-    # Momentum convention is the same as the frequency convention
-
     if Ch_from === Ch_to
-        return Ω, ν, νp
+        return Q, k, kp
 
     elseif Ch_from === pCh && Ch_to === tCh
-        return Ω - ν - νp, νp, ν
+        return Q - k - kp, kp, k
 
     elseif Ch_from === pCh && Ch_to === aCh
-        return ν - νp, Ω - ν, νp
+        return k - kp, Q - k, kp
 
     elseif Ch_from === tCh && Ch_to === pCh
-        return Ω + ν + νp, νp, ν
+        return Q + k + kp, kp, k
 
     elseif Ch_from === tCh && Ch_to === aCh
-        return νp - ν, Ω + ν, ν
+        return kp - k, Q + k, k
 
     elseif Ch_from === aCh && Ch_to === pCh
-        return Ω + νp + ν, Ω + νp, νp
+        return Q + kp + k, Q + kp, kp
 
     elseif Ch_from === aCh && Ch_to === tCh
-        return ν - νp, νp, Ω + νp
+        return k - kp, kp, Q + kp
 
     else
-        throw(ArgumentError("Wrong channels $Ch_from or $Ch_to"))
+        throw(ArgumentError("Cannot convert from $Ch_from to $Ch_to!"))
     end
-
 end
--- src/types.jl
+++ src/types.jl
@@ -1,46 +1,111 @@
-# channels
+# diagrammatic channels
+#----------------------------------------------------------------------------------------------#
+
+"""
+    abstract type ChannelTag end
+
+Abstract type for diagrammatic channels
+"""
 abstract type ChannelTag end
+
+"""
+    struct pCh <: ChannelTag end
+
+Parallel channel
+"""
 struct pCh <: ChannelTag end
+
+"""
+    struct tCh <: ChannelTag end
+
+Transversal channel
+"""
 struct tCh <: ChannelTag end
+
+"""
+    struct aCh <: ChannelTag end
+
+Antiparallel channel
+"""
 struct aCh <: ChannelTag end
 
 # spin components
+#----------------------------------------------------------------------------------------------#
+    
+"""
+    abstract type SpinTag end
+
+Abstract type for spin components
+"""
 abstract type SpinTag end
-struct pSp <: SpinTag end  # parallel
-struct xSp <: SpinTag end  # crossed
-struct dSp <: SpinTag end  # density
+
+"""
+    struct pSp <: SpinTag end
+
+Parallel spin component
+"""
+struct pSp <: SpinTag end
+
+"""
+    struct xSp <: SpinTag end
+
+Crossed spin component
+"""
+struct xSp <: SpinTag end
+
+"""
+    struct dSp <: SpinTag end
+
+Density component
+"""
+struct dSp <: SpinTag end
 
 # Mesh aliases
+#----------------------------------------------------------------------------------------------#
+    
 const FMesh = Mesh{MeshPoint{MatsubaraFrequency{Fermion}}, MatsubaraDomain}
 const BMesh = Mesh{MeshPoint{MatsubaraFrequency{Boson}}, MatsubaraDomain}
 const KMesh = Mesh{MeshPoint{BrillouinPoint{2}}, BrillouinDomain{2, 4}}
 
-# MeshFunction aliases : Local
+# MeshFunction aliases : local
+#----------------------------------------------------------------------------------------------#
+
 const MF_G{Q}  = MeshFunction{1, Q, Tuple{FMesh}, Array{Q, 1}}
 const MF_Π{Q}  = MeshFunction{2, Q, Tuple{BMesh, FMesh}, Array{Q, 2}}
 const MF_K1{Q} = MeshFunction{1, Q, Tuple{BMesh}, Array{Q, 1}}
 const MF_K2{Q} = MeshFunction{2, Q, Tuple{BMesh, FMesh}, Array{Q, 2}}
 const MF_K3{Q} = MeshFunction{3, Q, Tuple{BMesh, FMesh, FMesh}, Array{Q, 3}}
 
-# MeshFunction aliases : Nonlocal Green function and bubble
-const NL_MF_G{Q}  = MeshFunction{2, Q, Tuple{FMesh, KMesh}, Array{Q, 2}}
-const NL_MF_Π{Q}  = MeshFunction{4, Q, Tuple{BMesh, FMesh, KMesh, KMesh}, Array{Q, 4}}
+# MeshFunction aliases : nonlocal Green's function and bubble
+#----------------------------------------------------------------------------------------------#
 
-# MeshFunction aliases : Nonlocal vertex with only bosonic frequency dependence
+const NL_MF_G{Q} = MeshFunction{2, Q, Tuple{FMesh, KMesh}, Array{Q, 2}}
+const NL_MF_Π{Q} = MeshFunction{4, Q, Tuple{BMesh, FMesh, KMesh, KMesh}, Array{Q, 4}}
+
+# MeshFunction aliases : nonlocal vertex with only bosonic frequency dependence
+#----------------------------------------------------------------------------------------------#
+    
 const NL_MF_K1{Q} = MeshFunction{2, Q, Tuple{BMesh, KMesh}, Array{Q, 2}}
 const NL_MF_K2{Q} = MeshFunction{3, Q, Tuple{BMesh, FMesh, KMesh}, Array{Q, 3}}
 const NL_MF_K3{Q} = MeshFunction{4, Q, Tuple{BMesh, FMesh, FMesh, KMesh}, Array{Q, 4}}
 
-# MeshFunction aliases : Nonlocal vertex with bosonic and fermionic frequency dependences
+# MeshFunction aliases : nonlocal vertex with bosonic and fermionic frequency dependences
+#----------------------------------------------------------------------------------------------#
+
 const NL2_MF_K2{Q} = MeshFunction{4, Q, Tuple{BMesh, FMesh, KMesh, KMesh}, Array{Q, 4}}
 
+# struct to describe high-frequency limit
+#----------------------------------------------------------------------------------------------#
 
-struct InfiniteMatsubaraFrequency; end
+struct InfiniteMatsubaraFrequency end
 const νInf = InfiniteMatsubaraFrequency()
 
 Base.:+(::MatsubaraFrequency{Boson}, ::InfiniteMatsubaraFrequency) = InfiniteMatsubaraFrequency()
 Base.:-(::MatsubaraFrequency{Boson}, ::InfiniteMatsubaraFrequency) = InfiniteMatsubaraFrequency()
 Base.:+(::InfiniteMatsubaraFrequency, ::MatsubaraFrequency{Boson}) = InfiniteMatsubaraFrequency()
 Base.:-(::InfiniteMatsubaraFrequency, ::MatsubaraFrequency{Boson}) = InfiniteMatsubaraFrequency()
+
+# origin of the Brillouin zone
+#----------------------------------------------------------------------------------------------#
 
 const k0 = BrillouinPoint(0, 0)
