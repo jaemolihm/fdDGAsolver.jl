036134fa1f2ebb772de63d546480c6f89a011f16
Jae-Mo Lihm <jaemolim96@gmail.com>
2024-04-21 17:22:05 +0200

    Update mfRG

--- src/mfRG.jl
+++ src/mfRG.jl
@@ -1,7 +1,8 @@
-struct mfRGLinearMap{ST, Q} <: LinearMaps.LinearMap{Q}
-    S :: ST
+mutable struct mfRGLinearMap{ST, Q} <: LinearMaps.LinearMap{Q}
+    const S :: ST
+    is_first_iteration :: Bool
     function mfRGLinearMap(S :: ST) where {ST <: AbstractSolver{Q}} where {Q}
-        new{ST, Q}(S)
+        new{ST, Q}(S, true)
     end
 end
 
@@ -12,8 +13,11 @@ function LinearMaps._unsafe_mul!(y, A::mfRGLinearMap, x::AbstractVector)
 
     unflatten!(S.F, x)
 
-    build_K3_cache_mfRG!(S)
+    # Takes ~30% of the time
+    build_K3_cache_mfRG!(S, A.is_first_iteration)
+    A.is_first_iteration = false
 
+    # Takes ~30% of the time
     BSE_L_K2!(S, pCh)
     BSE_L_K2!(S, aCh)
     BSE_L_K2!(S, tCh)
@@ -28,6 +32,7 @@ function LinearMaps._unsafe_mul!(y, A::mfRGLinearMap, x::AbstractVector)
     BSE_K1_mfRG!(S, aCh)
     BSE_K1_mfRG!(S, tCh)
 
+    # Takes ~30% of the time
     BSE_K2_mfRG!(S, pCh)
     BSE_K2_mfRG!(S, aCh)
     BSE_K2_mfRG!(S, tCh)
@@ -38,35 +43,11 @@ function LinearMaps._unsafe_mul!(y, A::mfRGLinearMap, x::AbstractVector)
 
     set!(S.F, S.Fbuff)
 
-    y .= flatten(S.F) .- x
+    flatten!(S.F, y)
+    y .-= x
 
     return y
 end
-
-
-# julia> @time mfRGLinearMap(S) * flatten(S.F);
-#   0.174776 seconds (123 allocations: 12.672 KiB)
-#
-#   0.073509 seconds (9.89 k allocations: 624.344 KiB)
-#   0.074145 seconds (9.89 k allocations: 624.344 KiB)
-#   0.136028 seconds (9.89 k allocations: 624.344 KiB)
-#
-#   0.006366 seconds (4.58 k allocations: 291.469 KiB)
-#   0.006095 seconds (4.58 k allocations: 291.469 KiB)
-#   0.005945 seconds (4.58 k allocations: 291.469 KiB)
-#
-#   0.012447 seconds (702 allocations: 33.219 KiB)
-#   0.011846 seconds (702 allocations: 33.219 KiB)
-#   0.025590 seconds (702 allocations: 33.219 KiB)
-#
-#   0.241593 seconds (9.89 k allocations: 624.344 KiB)
-#   0.225840 seconds (9.89 k allocations: 624.344 KiB)
-#   0.481961 seconds (9.89 k allocations: 624.344 KiB)
-#
-#   0.003829 seconds (2.72 k allocations: 175.219 KiB)
-#   0.003710 seconds (2.72 k allocations: 175.219 KiB)
-#   0.003541 seconds (2.72 k allocations: 175.219 KiB)
-#   1.539046 seconds (83.96 k allocations: 31.851 MiB, 0.21% gc time)
 
 
 # fixed-point equation
@@ -144,7 +125,15 @@ function fixed_point_self_energy!(
     return nothing
 end
 
-function solve_using_mfRG_mix_bubble!(
+
+"""
+# Adaptive mixing
+Adaptively tune mixing to converge the fdDΓA calculation.
+Set ``Π = mixing * Π(G) + mixing * Π(G0)`` and run fdDΓA.
+If fdDΓA is converged, increase mixing by 20% and proceed.
+If fdDΓA is not converged, reduce mixing by 50% and retry.
+"""
+function solve_using_mfRG!(
     S :: AbstractSolver
     ;
     filename_log = nothing,
@@ -152,111 +141,7 @@ function solve_using_mfRG_mix_bubble!(
     verbose = true,
     occ_target = nothing,
     hubbard_params = nothing,
-    bubble_mixing = 1.0,
-    )
-
-    for iter in 1:maxiter
-        # Mix bubble
-        @. S.Πph.data = S.Πph.data * bubble_mixing + S.Π0ph.data * (1 - bubble_mixing);
-        @. S.Πpp.data = S.Πpp.data * bubble_mixing + S.Π0pp.data * (1 - bubble_mixing);
-
-        if mpi_ismain()
-            println(" === Iteration $iter ===")
-            println("Bubble mixing by $bubble_mixing")
-            println("max|Πpp - Π0pp| = $(absmax(S.Πpp - S.Π0pp))")
-            println("max|Πph - Π0ph| = $(absmax(S.Πph - S.Π0ph))")
-        end
-
-        # Solve vertex
-        if mpi_ismain()
-            println("Solving vertex")
-        end
-        kwargs_solver_vertex = (; update_Σ = false, strategy = :fdPA)
-        @time res = nlsolve((R, x) -> fixed_point_preconditioned!(R, x, S; kwargs_solver_vertex...), flatten(S.F),
-            method = :anderson,
-            iterations = 200,
-            ftol = 1e-4,
-            beta = 0.85,
-            m = 100,
-            show_trace = mpi_ismain() && verbose,
-        );
-
-        unflatten!(S.F, res.zero);
-
-        # Store current target for the update after SDE
-        Πpp_copy = copy(S.Πpp)
-        Πph_copy = copy(S.Πph)
-        G_copy = copy(S.G)
-
-        SDE!(S; strategy = :fdPA)
-        Σ_copy = copy(S.Σ)
-
-
-        # Solve self-energy
-        if mpi_ismain()
-            println("Solving self-energy")
-        end
-        kwargs_solver_selfen = (; strategy = :fdPA, occ_target, hubbard_params)
-        @time res = nlsolve((R, x) -> fixed_point_self_energy!(R, x, S; kwargs_solver_selfen...), flatten(S.Σ),
-            method = :anderson,
-            iterations = 100,
-            ftol = 1e-4,
-            beta = 0.85,
-            m = 10,
-            show_trace = mpi_ismain() && verbose,
-        );
-
-        unflatten!(S.Σ, res.zero);
-
-        # Update fdPA reference
-
-        # Update reference Green fucntion, bubble, and self-energy
-        set!(S.Π0pp, Πpp_copy)
-        set!(S.Π0ph, Πph_copy)
-        set!(S.G0, G_copy)
-        set!(S.Σ0, Σ_copy)
-
-        # Update reference vertex and reset target vertex
-        add!(S.F0, S.F)
-        set!(S.F, 0)
-
-        # Update target Green function and bubble
-        Dyson!(S)
-        bubbles!(S)
-
-        flush(stdout)
-        flush(stderr)
-
-        if filename_log !== nothing
-            if mpi_ismain()
-                h5open(filename_log * ".iter$iter.h5", "w") do f
-                    save!(f, "S", S)
-                end
-            end
-        end
-
-
-        if mpi_ismain()
-            err = absmax(S.Σ - S.Σ0)
-            println("Self-energy error: $err")
-
-            if err < 1e-4
-                break
-            end
-        end
-    end
-end
-
-
-function solve_using_mfRG_mix_G!(
-    S :: AbstractSolver
-    ;
-    filename_log = nothing,
-    maxiter = 100,
-    verbose = true,
-    occ_target = nothing,
-    hubbard_params = nothing,
-    mixing_G_init = 1.0,
+    mixing_init = 1.0,
     iter_restart = 0,
     tol = 1e-4,
     auto_restart = false,
@@ -269,13 +154,13 @@ function solve_using_mfRG_mix_G!(
         end
     end
 
-    mixing_G = mixing_G_init
-
     if iter_restart !== 0
         # Restart from file
+        iter = iter_restart
         filename = "$filename_log.iter$iter_restart.h5"
         mpi_ismain() && println("Restarting from $filename")
         f = h5open(filename, "r")
+        set!(S.Gbare,  load_mesh_function(f, "Gbare"))
         set!(S.G,  load_mesh_function(f, "G"))
         set!(S.Σ,  load_mesh_function(f, "Σ"))
         set!(S.G0, load_mesh_function(f, "G0"))
@@ -284,30 +169,35 @@ function solve_using_mfRG_mix_G!(
         set!(S.Π0ph, load_mesh_function(f, "Π0ph"))
         set!(S.Πpp, load_mesh_function(f, "Πpp"))
         set!(S.Πph, load_mesh_function(f, "Πph"))
-        set!(S.F,  fdDGAsolver.load_vertex(NL2_Vertex, f, "F"))
-        set!(S.F0, fdDGAsolver.load_vertex(NL2_Vertex, f, "F0"))
+        set!(S.F,  load_vertex(NL2_Vertex, f, "F"))
+        set!(S.F0, load_vertex(NL2_Vertex, f, "F0"))
+        mixing = read(f, "mixing")
         close(f)
+    else
+        mixing = mixing_init
+        iter = 0
     end
 
-    iter = iter_restart
 
     for iter_ in 1:maxiter
         iter += 1
 
-        # Mix Green function
-        G_copy = copy(S.G)
-        set!(S.G, S.G * mixing_G + S.G0 * (1 - mixing_G))
-        bubbles!(S)
+        # Mix bubble
+        # Here, S.Π are the unmixed bubble, and S.Π0 is the mixed bubble from the previous iteration.
+        Πpp_mixed = S.Πpp * mixing + S.Π0pp * (1 - mixing)
+        Πph_mixed = S.Πph * mixing + S.Π0ph * (1 - mixing)
+        set!(S.Πpp, Πpp_mixed)
+        set!(S.Πph, Πph_mixed)
 
-        if mpi_ismain()
+        if mpi_ismain() && verbose
             println(" === Iteration $iter (including retry: $iter_) ===")
-            println("G mixing by $mixing_G")
+            println("Mix Π and Π0 by $mixing")
             println("max|Πpp - Π0pp| = $(absmax(S.Πpp - S.Π0pp))")
             println("max|Πph - Π0ph| = $(absmax(S.Πph - S.Π0ph))")
         end
 
         # Solve vertex
-        if mpi_ismain()
+        if mpi_ismain() && verbose
             println("Solving vertex")
         end
         kwargs_solver_vertex = (; update_Σ = false, strategy = :fdPA)
@@ -315,34 +205,38 @@ function solve_using_mfRG_mix_G!(
             method = :anderson,
             iterations = 200,
             ftol = tol,
-            beta = 1.0,
+            beta = 0.85,
             m = 100,
             show_trace = mpi_ismain() && verbose,
         );
 
         if ! res.f_converged
             # If the calculation did not converge, reduce mixing and retry.
-            mixing_G /= 2.0
-            set!(S.F, 0)
-            set!(S.G, G_copy)
+            mixing /= 2.0
             iter -= 1
-            mpi_ismain() && println("Calculation did not converge in $(res.iterations) iterations. Reduce mixing_G to $mixing_G")
+            bubbles!(S)  # Reset bubbles to the unmixed value
+
+            mpi_ismain() && println("Calculation did not converge in $(res.iterations) iterations. Reduce mixing to $mixing")
             continue
         else
-            # If the calculation converged, increate mixing.
-            mixing_G = min(1.0, mixing_G * 1.2)
-            mpi_ismain() && println("Calculation converged, increase mixing_G to $mixing_G")
+            # If the calculation converged, increase mixing.
+            mixing = min(1.0, mixing * 1.2)
+            mpi_ismain() && println("Calculation converged, increase mixing to $mixing")
         end
         unflatten!(S.F, res.zero);
 
+        # Restore unmixed bubble for SDE
+        bubbles_real_space!(S.Π0pp, S.Π0ph, S.G0)
+        bubbles_real_space!(S.Πpp, S.Πph, S.G)
+
         # Update self-energy
         SDE!(S; strategy = :fdPA)
-        Σ_err = absmax(S.Σ - S.Σ0) / mixing_G
+        Σ_err = absmax(S.Σ - S.Σ0) / mixing
 
 
-        # Update reference Green fucntion, bubble, and self-energy
-        set!(S.Π0pp, S.Πpp)
-        set!(S.Π0ph, S.Πph)
+        # Update reference Green function, bubble, and self-energy
+        set!(S.Π0pp, Πpp_mixed)
+        set!(S.Π0ph, Πph_mixed)
         set!(S.G0, S.G)
         set!(S.Σ0, S.Σ)
 
@@ -368,6 +262,7 @@ function solve_using_mfRG_mix_G!(
             if mpi_ismain()
                 h5open(filename_log * ".iter$iter.h5", "w") do f
                     save!(f, "S", S)
+                    f["mixing"] = mixing
                 end
             end
         end
